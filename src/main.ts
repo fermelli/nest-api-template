f5b251e1ab63ef1290a996ae9ecd544100e5b93b
fermelli <luisfernandosalgadomiguez@gmail.com>
2023-07-09 00:37:31 -0400

    uso variables de configuracion

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(
+    configService.get<number>('PORT', 3000),
+    configService.get<string>('HOST', 'localhost'),
+  );
 }
+
 bootstrap();
