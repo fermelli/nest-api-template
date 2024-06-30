ca6601b565b81f46ec44cbd5e1d8b97cd05ee62e
fermelli <luisfernandosalgadomiguez@gmail.com>
2024-06-30 02:53:20 -0400

    refactorizar validacion del campo de name en UpdateRoleDto para corregir error al actualizar un rol

--- src/roles/dto/update-role.dto.ts
+++ src/roles/dto/update-role.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRoleDto } from './create-role.dto';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
-export class UpdateRoleDto extends PartialType(CreateRoleDto) {}
+export class UpdateRoleDto extends PartialType(CreateRoleDto) {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(64)
+  name: string;
+}
