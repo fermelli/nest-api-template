c6f16e9092fa88d6f6d870eb38bd53ea0fc48b98
fermelli <luisfernandosalgadomiguez@gmail.com>
2025-06-02 17:37:14 -0400

    eliminar AppController y AppService y limpiar el modulo de la aplicacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
@@ -26,7 +24,7 @@ import validate from './app/enviroment/enviroment.validation';
     RolesModule,
     PermissionsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
