9d5a6eb9489adcf7b0a4c7a4a9fe5a00c5b2cc5d
fermelli <luisfernandosalgadomiguez@gmail.com>
2025-06-29 18:10:20 -0400

    agregar validacion de existencia para el campo id en PermissionsDto

--- src/permissions/dtos/permissions.dto.ts
+++ src/permissions/dtos/permissions.dto.ts
@@ -1,9 +1,12 @@
 import { Type } from 'class-transformer';
 import { IsInt, IsNotEmpty } from 'class-validator';
+import { Exists } from 'src/common/decorators/exists.decorator';
+import { Permission } from '../entities/permission.entity';
 
 export class PermissionsDto {
   @Type(() => Number)
   @IsNotEmpty()
   @IsInt()
+  @Exists({ entity: Permission, column: 'id' })
   id: number;
 }
