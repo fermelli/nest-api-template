1c86e5db7db003c1ad881d252b2c3257669f24c9
fermelli <luisfernandosalgadomiguez@gmail.com>
2025-06-29 17:43:09 -0400

    agregar validacion de existencia para el campo id en RolesDto

--- src/roles/dto/roles.dto.ts
+++ src/roles/dto/roles.dto.ts
@@ -1,9 +1,12 @@
 import { Type } from 'class-transformer';
 import { IsNotEmpty, IsInt } from 'class-validator';
+import { Exists } from 'src/common/decorators/exists.decorator';
+import { Role } from '../entities/role.entity';
 
 export class RolesDto {
   @Type(() => Number)
   @IsNotEmpty()
   @IsInt()
+  @Exists({ entity: Role, column: 'id' })
   id: number;
 }
